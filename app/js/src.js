3f6fe6a1293d59f8d5860609605771d492bc6cfb
unknown <jvanwagenen@JVANWAGENEN.mainman.dcs>
2014-11-18 13:39:40 -0700

    increasing height of frames

--- app/js/src.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-	$(document).scroll(function() {
-		var scrollPos = $(window).scrollTop(),
-			max = $(document).height() - $(window).height(), // Max avilable scroll position
-			header = $('#nav');
-
-		header.css('opacity', 1 - (scrollPos / max));
-	});
-});
